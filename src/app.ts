c499c2ac18bb6bd32589049d6d187df66f74d4f7
n0rrman <email@henriknorrman.com>
2024-05-19 22:33:09 +0700

    middleware troubleshooting

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,8 @@ import { consonantRouter } from "./routes/consonant";
 
 export const app = new Koa();
 
+let x = 0;
+
+app.use((ctx) => {console.log(ctx, x++)});
 app.use(consonantRouter.routes());
+app.use(consonantRouter.allowedMethods());
