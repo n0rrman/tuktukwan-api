515e731a569359d09b2d0183efe55a72e0771dbf
n0rrman <email@henriknorrman.com>
2024-05-19 23:02:11 +0700

    bug fixed

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,5 @@ import { consonantRouter } from "./routes/consonant";
 
 export const app = new Koa();
 
-let x = 0;
-
-app.use((ctx) => {console.log(ctx, x++)});
 app.use(consonantRouter.routes());
 app.use(consonantRouter.allowedMethods());
--- src/routes/consonant.ts
+++ src/routes/consonant.ts
@@ -3,6 +3,7 @@ import ConsonantRepo from "../repos/consonant-repo";
 
 const router = new Router();
 
+
 router.get('/api/consonant', async (ctx, next) => { 
     try {
         const data = await ConsonantRepo.find();
@@ -16,13 +17,6 @@ router.get('/api/consonant', async (ctx, next) => {
     await next();
 });
 
-router.get('/api', async (ctx, next) => { 
-    console.log("/api")
-    ctx.status = 200;
-    ctx.body = "/api"
-    
-    await next();
-});
 
 router.get('/', async (ctx, next) => { 
     console.log("/")
